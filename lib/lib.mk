1f0328a7f5c24e7dd49a81c2cf5a8f0ed81f465e
Tim Menzies <menzies.tim@gmail.com>
2015-01-07 02:52:45 +0000

    isaving

--- lib/lib.mk
+++ lib/lib.mk
@@ -57,7 +57,7 @@ files:
 talks:  $(call target,slides,md,html,$(Raw),$(Out))
 dots  : $(call target,dot,dot,png,$(Raw),$(Out)/img)
 plots : $(call target,plot,plt,png,$(Raw),$(Out)/img)
-pages : $(call target,posts,md,html,$(Raw),$(Out))
+pages : $(call target,posts,md,html,$(Raw),$(Out)/..)
 
 $(Out)/slides/%.html : $(Raw)/slides/%.md
 	pandoc -s \
