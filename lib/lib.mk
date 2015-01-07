d220888b3b2eb3045e47ba74c0e7e9ffa537e080
Tim Menzies <tim@menzies.us>
2015-01-06 22:20:03 -0500

    typo

--- lib/lib.mk
+++ lib/lib.mk
@@ -60,7 +60,7 @@ dots  : $(call target,dot,dot,png,$(Raw),$(Out)/img)
 plots : $(call target,plot,plt,png,$(Raw),$(Out)/img)
 pages : $(call target,posts,md,html,$(Raw),$(Out))
 
-$(Out)/slides/%.html : $(Raw)/slides/%.md
+$(Out)/slides/%.html : $(Raw)/slides/%.md 
 	pandoc -s \
               --webtex -i -t slidy \
               -r markdown+simple_tables+table_captions \
